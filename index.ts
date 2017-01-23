4dbaa257b3345c51603a7302801823d8d8187ac1
Derek P Sifford <dereksifford@gmail.com>
2017-01-23 13:27:02 -0500

    progress on the PaaS move

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const controller: Botkit.Controller = Botkit.slackbot({
-    debug: true,
+    debug: process.env.NODE_ENV !== 'production',
 });
 
 controller.spawn({ token: TOKEN }).startRTM((err, bot, payload) => {
